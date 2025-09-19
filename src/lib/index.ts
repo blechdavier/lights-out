8db55f75d88f927a6279514774f4b44d22f3de06
Xavier Bradford <73443592+blechdavier@users.noreply.github.com>
2025-09-18 17:09:40 -0700

    faster lol

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,100 +1,149 @@
 export function generateSolvableBoard(size: number): boolean[] {
-    while (true) {
-        let arr = generateRandomBoard(size);
-        let toggles = generateToggleMatrix(size);
-        let solution = solveLinearSystem(toggles, arr);
-        if (solution != "unsolvable") {
-            return arr;
-        }
+  // while (true) {
+  //   let arr = generateRandomBoard(size);
+  //   let toggles = generateToggleMatrix(size);
+  //   let solution = solveLinearSystem(toggles, arr);
+  //   if (solution != "unsolvable") {
+  //     return arr;
+  //   }
+  // }
+  let arr = generateBlankBoard(size);
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      if (Math.random() < 0.5) {
+        toggleSquare(arr, size, i, j);
+      }
+      // res.push(true);
     }
+  }
+  return arr;
 }
 
-function generateRandomBoard(size: number): boolean[] {
-    let res: boolean[] = []
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-            res.push(Math.random() < 0.5);
-            // res.push(true);
-        }
-    }
-    return res;
+export function toggleSquare(
+  board: boolean[],
+  size: number,
+  x: number,
+  y: number,
+) {
+  board[y * size + x] = !board[y * size + x];
+  if (x != 0) {
+    board[y * size + x - 1] = !board[y * size + x - 1];
+  }
+  if (x != size - 1) {
+    board[y * size + x + 1] = !board[y * size + x + 1];
+  }
+  if (y != 0) {
+    board[(y - 1) * size + x] = !board[(y - 1) * size + x];
+  }
+  if (y != size - 1) {
+    board[(y + 1) * size + x] = !board[(y + 1) * size + x];
+  }
 }
 
-export function solveLinearSystem(matrix: boolean[][], vector: boolean[]): boolean[] | "unsolvable" {
-    const size = matrix.length;
-    let m = matrix;
-    for (let [i, val] of vector.entries()) {
-        m[i].push(val);
-    }
-    for (let i = 0; i < size; i++) {
-        let trueIndex;
-        for (let j = i; j < size; j++) {
-            if (m[j][i]) {
-                trueIndex = j; break;
-            }
-        }
-        if (trueIndex == undefined) {
-            for (let j = i; j < size; j++) {
-                if (m[j].includes(true)) {
-                    // console.log("this system is not solvable. 1 in every " + Math.pow(2, size - i) + " random boards are solvable.");
-                    return "unsolvable";
-                }
-            }
-            // TODO find the solution with the least button presses. 
-            // console.log("this system is solvable " + Math.pow(2, size - i) + " ways.");
-            break;
-        }
-        swapRows(m, i, trueIndex);
-        for (let j = 0; j < size; j++) {
-            if (i == j) continue; // don't add to self :)
-            if (m[j][i]) {
-                addRows(m, i, j);
-            }
-        }
-    }
+// function generateRandomBoard(size: number): boolean[] {
+//   let res: boolean[] = [];
+//   for (let i = 0; i < size; i++) {
+//     for (let j = 0; j < size; j++) {
+//       res.push(Math.random() < 0.5);
+//       // res.push(true);
+//     }
+//   }
+//   return res;
+// }
 
-    let res: boolean[] = [];
-    for (let i = 0; i < size; i++) {
-        res.push(m[i][size]);
+function generateBlankBoard(size: number): boolean[] {
+  let res: boolean[] = [];
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      res.push(false);
     }
-    return res;
+  }
+  return res;
+}
+
+export function solveLinearSystem(
+  matrix: boolean[][],
+  vector: boolean[],
+): boolean[] | "unsolvable" {
+  const size = matrix.length;
+  let m = matrix;
+  for (let [i, val] of vector.entries()) {
+    m[i].push(val);
+  }
+  for (let i = 0; i < size; i++) {
+    let trueIndex;
+    for (let j = i; j < size; j++) {
+      if (m[j][i]) {
+        trueIndex = j;
+        break;
+      }
+    }
+    if (trueIndex == undefined) {
+      for (let j = i; j < size; j++) {
+        if (m[j].includes(true)) {
+          console.log(
+            "this system is not solvable. 1 in every " +
+              Math.pow(2, size - i) +
+              " random boards are solvable.",
+          );
+          return "unsolvable";
+        }
+      }
+      // TODO find the solution with the least button presses.
+      // console.log("this system is solvable " + Math.pow(2, size - i) + " ways.");
+      break;
+    }
+    swapRows(m, i, trueIndex);
+    for (let j = 0; j < size; j++) {
+      if (i == j) continue; // don't add to self :)
+      if (m[j][i]) {
+        addRows(m, i, j);
+      }
+    }
+  }
+
+  let res: boolean[] = [];
+  for (let i = 0; i < size; i++) {
+    res.push(m[i][size]);
+  }
+  return res;
 }
 
 function swapRows(matrix: boolean[][], index1: number, index2: number) {
-    const tmp = matrix[index1];
-    matrix[index1] = matrix[index2];
-    matrix[index2] = tmp;
+  const tmp = matrix[index1];
+  matrix[index1] = matrix[index2];
+  matrix[index2] = tmp;
 }
 
 function addRows(matrix: boolean[][], index1: number, index2: number) {
-    for (let i = 0; i < matrix[index1].length; i++) {
-        if (matrix[index1][i]) {
-            // 1... flip the bit in index2
-            matrix[index2][i] = !matrix[index2][i];
-        }
+  for (let i = 0; i < matrix[index1].length; i++) {
+    if (matrix[index1][i]) {
+      // 1... flip the bit in index2
+      matrix[index2][i] = !matrix[index2][i];
     }
+  }
 }
 
 export function generateToggleMatrix(size: number): boolean[][] {
-    let res: boolean[][] = [];
-    for (let x1 = 0; x1 < size; x1++) {
-        for (let y1 = 0; y1 < size; y1++) {
-            let element: boolean[] = [];
-            for (let x2 = 0; x2 < size; x2++) {
-                for (let y2 = 0; y2 < size; y2++) {
-                    // probably could clean this up...
-                    if (x1 == x2 && ((y1 == y2 - 1) || y1 == y2 || (y1 == y2 + 1))) {
-                        element.push(true);
-                    } else if (y1 == y2 && ((x1 == x2 - 1) || x1 == x2 || (x1 == x2 + 1))) {
-                        element.push(true);
-                    } else {
-                        element.push(false);
-                    }
-                    // element.push("(" + x2 + "," + y2 + ")");
-                }
-            }
-            res.push(element);
+  let res: boolean[][] = [];
+  for (let x1 = 0; x1 < size; x1++) {
+    for (let y1 = 0; y1 < size; y1++) {
+      let element: boolean[] = [];
+      for (let x2 = 0; x2 < size; x2++) {
+        for (let y2 = 0; y2 < size; y2++) {
+          // probably could clean this up...
+          if (x1 == x2 && (y1 == y2 - 1 || y1 == y2 || y1 == y2 + 1)) {
+            element.push(true);
+          } else if (y1 == y2 && (x1 == x2 - 1 || x1 == x2 || x1 == x2 + 1)) {
+            element.push(true);
+          } else {
+            element.push(false);
+          }
+          // element.push("(" + x2 + "," + y2 + ")");
         }
+      }
+      res.push(element);
     }
-    return res;
+  }
+  return res;
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,129 +1,119 @@
 <script lang="ts">
-  import {
-    generateSolvableBoard,
-    generateToggleMatrix,
-    solveLinearSystem,
-  } from "$lib";
+    import {
+        generateSolvableBoard,
+        generateToggleMatrix,
+        solveLinearSystem,
+        toggleSquare,
+    } from "$lib";
 
-  let showAnswers = false;
-  let size = 3;
-  let board = generateSolvableBoard(size);
-  let toggles = generateToggleMatrix(size);
-  let solution = solveLinearSystem(toggles, board);
+    let showAnswers = false;
+    let size = 3;
+    let board = generateSolvableBoard(size);
+    let toggles = generateToggleMatrix(size);
+    let solution = solveLinearSystem(toggles, board);
 
-  function regenBoard() {
-    board = generateSolvableBoard(size);
-    toggles = generateToggleMatrix(size);
-    solution = solveLinearSystem(toggles, board);
-  }
-
-  function toggleSquare(x: number, y: number) {
-    board[y * size + x] = !board[y * size + x];
-    if (x != 0) {
-      board[y * size + x - 1] = !board[y * size + x - 1];
+    function regenBoard() {
+        board = generateSolvableBoard(size);
+        toggles = generateToggleMatrix(size);
+        solution = solveLinearSystem(toggles, board);
     }
-    if (x != size - 1) {
-      board[y * size + x + 1] = !board[y * size + x + 1];
-    }
-    if (y != 0) {
-      board[(y - 1) * size + x] = !board[(y - 1) * size + x];
-    }
-    if (y != size - 1) {
-      board[(y + 1) * size + x] = !board[(y + 1) * size + x];
-    }
-    toggles = generateToggleMatrix(size);
-    solution = solveLinearSystem(toggles, [...board]);
-    console.log([...solution]);
-  }
 </script>
 
 <div class="flex flex-col items-center justify-center h-screen">
-  <h1 class="text-4xl font-black inline-block font-title mb-2">
-    Lights Out 💡
-  </h1>
-  <p class="text-gray-600 mb-8">
-    Turn all the lights out to win. Press any light to begin.
-  </p>
-  <div class="space-y-1 mb-8">
-    {#each { length: size }, i}
-      <div class="flex space-x-1">
-        {#each board.slice(i * size, i * size + size) as x, j}
-          <button
-            class="flex items-center justify-center w-8 h-8 rounded"
-            class:bg-yellow-500={x}
-            class:bg-gray-300={!x}
-            on:click={() => toggleSquare(j, i)}
-          >
-            {#if showAnswers && solution[i * size + j]}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                class:text-white={x}
-                class:text-black={!x}
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="icon icon-tabler icons-tabler-outline icon-tabler-click"
-                ><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
-                  d="M3 12l3 0"
-                /><path d="M12 3l0 3" /><path d="M7.8 7.8l-2.2 -2.2" /><path
-                  d="M16.2 7.8l2.2 -2.2"
-                /><path d="M7.8 16.2l-2.2 2.2" /><path
-                  d="M12 12l9 3l-4 2l-2 4l-3 -9"
-                /></svg
-              >
-            {/if}
-          </button>
+    <h1 class="text-4xl font-black inline-block font-title mb-2">
+        Lights Out 💡
+    </h1>
+    <p class="text-gray-600 mb-8">
+        Turn all the lights out to win. Press any light to begin.
+    </p>
+    <div class="space-y-1 mb-8">
+        {#each { length: size }, i}
+            <div class="flex space-x-1">
+                {#each board.slice(i * size, i * size + size) as x, j}
+                    <button
+                        class="flex items-center justify-center w-8 h-8 rounded"
+                        class:bg-yellow-500={x}
+                        class:bg-gray-300={!x}
+                        on:click={() => {
+                            toggleSquare(board, size, j, i);
+                            toggles = generateToggleMatrix(size);
+                            solution = solveLinearSystem(toggles, [...board]);
+                        }}
+                    >
+                        {#if showAnswers && solution[i * size + j]}
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                                class:text-white={x}
+                                class:text-black={!x}
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="icon icon-tabler icons-tabler-outline icon-tabler-click"
+                                ><path
+                                    stroke="none"
+                                    d="M0 0h24v24H0z"
+                                    fill="none"
+                                /><path d="M3 12l3 0" /><path
+                                    d="M12 3l0 3"
+                                /><path d="M7.8 7.8l-2.2 -2.2" /><path
+                                    d="M16.2 7.8l2.2 -2.2"
+                                /><path d="M7.8 16.2l-2.2 2.2" /><path
+                                    d="M12 12l9 3l-4 2l-2 4l-3 -9"
+                                /></svg
+                            >
+                        {/if}
+                    </button>
+                {/each}
+            </div>
         {/each}
-      </div>
-    {/each}
-  </div>
-  <div class="flex space-x-2 items-center mb-2">
-    <select
-      class="bg-white rounded-full text-black border border-gray-300 font-medium text-sm px-5 py-2.5"
-      bind:value={size}
-      on:change={regenBoard}
-    >
-      {#each { length: 16 }, i}
-        <option value={i + 3}>{i + 3}x{i + 3} grid</option>
-      {/each}
-    </select>
-    <button
-      class="aspect-square p-2.5 bg-white rounded-full text-black border border-gray-300 font-medium text-sm hover:bg-gray-50"
-      aria-label="New Board"
-      on:click={regenBoard}
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="size-5"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-        />
-      </svg>
-    </button>
+    </div>
+    <div class="flex space-x-2 items-center mb-2">
+        <select
+            class="bg-white rounded-full text-black border border-gray-300 font-medium text-sm px-5 py-2.5"
+            bind:value={size}
+            on:change={regenBoard}
+        >
+            {#each { length: 1000 }, i}
+                <option value={i + 3}>{i + 3}x{i + 3} grid</option>
+            {/each}
+        </select>
+        <button
+            class="aspect-square p-2.5 bg-white rounded-full text-black border border-gray-300 font-medium text-sm hover:bg-gray-50"
+            aria-label="New Board"
+            on:click={regenBoard}
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-5"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                />
+            </svg>
+        </button>
 
-    <button
-      class="w-32 py-2.5 bg-white rounded-full text-black border border-gray-300 font-medium text-sm hover:bg-gray-50"
-      on:click={() => (showAnswers = !showAnswers)}
-    >
-      {showAnswers ? "Hide Solution" : "Show Solution"}
-    </button>
-  </div>
-  <p class="text-gray-600">
-    Solver (heavily) inspired by
-    <a class="underline" href="https://www.youtube.com/watch?v=1izbpSk3ays"
-      >this video</a
-    >.
-  </p>
+        <button
+            class="w-32 py-2.5 bg-white rounded-full text-black border border-gray-300 font-medium text-sm hover:bg-gray-50"
+            on:click={() => (showAnswers = !showAnswers)}
+        >
+            {showAnswers ? "Hide Solution" : "Show Solution"}
+        </button>
+    </div>
+    <p class="text-gray-600">
+        Solver (heavily) inspired by
+        <a class="underline" href="https://www.youtube.com/watch?v=1izbpSk3ays"
+            >this video</a
+        >.
+    </p>
 </div>
